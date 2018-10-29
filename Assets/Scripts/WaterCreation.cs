bbcb050322f5712aa13280d3f96fcb57c22c528d
Unknown <alex.keoghly@outlook.com>
2018-10-29 17:30:39 +0000

    First Upload

---
+++ Assets/Scripts/WaterCreation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCreation : MonoBehaviour {
+
+
+    public GameObject Water;
+
+	// Use this for initialization
+	void Start () {
+        WaterCreator();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void WaterCreator()
+    {
+        Instantiate(Water);
+    }
+}
