600ab75e18243a0a54e3b224f70a4d9891908387
Unknown <alex.keoghly@outlook.com>
2018-11-26 17:05:24 +0000

    UI Finished

---
+++ Assets/Scripts/Go_To_Level_One.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Go_To_Level_One : MonoBehaviour
+{
+    public void NextScene()
+    {
+        SceneManager.LoadScene("First_Level");
+    }
+}
---
+++ Assets/Scripts/Menu_Buttons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Buttons : MonoBehaviour {
+
+    public GameObject Menu_Panel;
+    public GameObject Level_Select_Panel;
+
+	// Use this for initialization
+	void Start () {
+        Menu_Panel.SetActive(true);
+        Level_Select_Panel.SetActive(false);
+	}
+
+    public void ShowLevelPanel()
+    {
+        Menu_Panel.SetActive(false);
+        Level_Select_Panel.SetActive(true);
+    }
+    public void ShowMenuPanel()
+    {
+        Menu_Panel.SetActive(true);
+        Level_Select_Panel.SetActive(false);
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
